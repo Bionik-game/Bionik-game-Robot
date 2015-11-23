40339ce8689be3b77c50ff69382c044e9c3c5dcd
fesky <feeroo@gmail.com>
2015-11-23 19:02:58 +0100

    dodanie testu RPC

---
+++ jsonclient/Debug/main.d
@@ -0,0 +1,3 @@
+main.d: ../main.cpp ../stubclient.h
+
+../stubclient.h:
---
+++ jsonclient/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -ljsonrpccpp-client -ljsonrpccpp-common -ljsoncpp
+
